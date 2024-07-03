8d3be36a50554c747b8b13fd4b74e82e9ecbc25f
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-07-03 16:35:12 +0200

    m-3 print in page 2 train with foreach

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Dati da "config/data.php": {{ $home }}</h1>
-<p>This is my paragraph content.</p>
-<button class="btn btn-primary">Cliccami</button>
+	<a href="{{ route('train') }}"><button class="btn btn-primary">Vai a Treni</button></a>
 @endsection
