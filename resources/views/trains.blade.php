402a3130820ebcc33ab5253dfed315e6e3f42c57
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-07-03 16:46:13 +0200

    migliorative layout

--- resources/views/trains.blade.php
+++ resources/views/trains.blade.php
@@ -1,12 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-	<ul>
-		@foreach ($trains as $train)
-			<li>
-				{{ $train->id }}: {{ $train->name }} {{ $train->departure_station }} {{ $train->arrival_station }}
-				{{ $train->departure_time }}
-			</li>
-		@endforeach
-	</ul>
+	@foreach ($trains as $train)
+		<div class="text-center">
+			<p>{{ $train->id }}: Treno</p>
+			<p>Nome. {{ $train->name }}</p>
+			<p>Stazione di Partenza: {{ $train->departure_station }}</p>
+			<p>Stazione di Arrivo: {{ $train->arrival_station }}</p>
+			<p>Orario di Partenza: {{ $train->departure_time }}</p>
+			<p>Orario di Arrivo: {{ $train->arrival_time }}</p>
+			<p>Codice Treno: {{ $train->train_code }}</p>
+			<p>Numero Carrozze Totali: {{ $train->number_carriages }}</p>
+			<p>In Orario? {{ $train->in_time }}</p>
+		</div>
+	@endforeach
 @endsection
